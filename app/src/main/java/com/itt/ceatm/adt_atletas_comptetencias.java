2e210cd0f4b21385150a2e47c2785eb28ea3ef36
adrubio <angel.rubio16@tectijuana.edu.mx>
2018-11-19 21:14:45 -0800

    Competencias Atleta, Diseño
    - Se agrego el layout, la clase del activity, el layout del recycler view, la clase del adaptador del recycler view y la clase que contiene los getter y setter para los datos de competencias
    
    - Se hicieron las referencias entre clases y ya se puede visualizar la parte de Competencias del Atleta con su Recycler View

---
+++ app/src/main/java/com/itt/ceatm/adt_atletas_comptetencias.java
@@ -0,0 +1,56 @@
+package com.itt.ceatm;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class adt_atletas_comptetencias extends RecyclerView.Adapter<adt_atletas_comptetencias.ViewHolderDatos>{
+
+    ArrayList<datos_atletas_competencias> lista_competencias;
+
+    public adt_atletas_comptetencias(ArrayList<datos_atletas_competencias> lista_competencias) {
+        this.lista_competencias = lista_competencias;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rv_atleta_competencias,null,false);
+        return new ViewHolderDatos(v);
+    }
+
+    //Establece la comunicacion entre el adaptador y la clase ViewHolderDatos
+    @Override
+    public void onBindViewHolder(@NonNull adt_atletas_comptetencias.ViewHolderDatos holder, int i) {
+        holder.comp.setText(lista_competencias.get(i).getCompetencia());
+        holder.punt1.setText(lista_competencias.get(i).getPuntaje1());
+        holder.punt2.setText(lista_competencias.get(i).getPuntaje2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return lista_competencias.size();
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder {
+        TextView comp,punt1,punt2;
+
+        public ViewHolderDatos(@NonNull View itemView) {
+            super(itemView);
+
+            comp = itemView.findViewById(R.id.atleta_competencia_competencia);
+            punt1 = itemView.findViewById(R.id.atleta_competencia_opcion1);
+            punt2 = itemView.findViewById(R.id.atleta_competencia_opcion2);
+        }
+
+        public void obtenerDatos(String s) {
+
+        }
+    }
+}
--- app/src/main/java/com/itt/ceatm/frg_atleta_competencias.java
+++ app/src/main/java/com/itt/ceatm/frg_atleta_competencias.java
@@ -4,14 +4,44 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class frg_atleta_competencias extends Fragment {
+
+    public ArrayList<datos_atletas_competencias> lista_comp;
+    public RecyclerView recycler_comp;
+    public adt_atletas_comptetencias adaptador_lista_competencias;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frg_atleta_competencias, container, false);
+        View view = inflater.inflate(R.layout.frg_atleta_competencias, container, false);
+
+        recycler_comp = (RecyclerView)view.findViewById(R.id.rcc_atleta_competencias);
+        recycler_comp.setLayoutManager(new LinearLayoutManager(this.getActivity(),LinearLayoutManager.VERTICAL,false));
+        //Grid
+        //recycler_comp.setLayoutManager(new GridLayoutManager(this.getActivity(),2));
+        data();
+        adaptador_lista_competencias = new adt_atletas_comptetencias(lista_comp);
+        recycler_comp.setAdapter(adaptador_lista_competencias);
+
+        return view;
     }
+
+    public void data(){
+        lista_comp = new ArrayList<>();
+        lista_comp.add(new datos_atletas_competencias("Juegos Transnacionales ", "8","15"));
+        lista_comp.add(new datos_atletas_competencias("Juegos Olimpicos de la Juventud 2018 ", "9","12"));
+        lista_comp.add(new datos_atletas_competencias("Juegos Juventud 2018", "12","11"));
+        lista_comp.add(new datos_atletas_competencias("Bala Alta 2018", "TKD","10"));
+        lista_comp.add(new datos_atletas_competencias("11vos Juegos Panamericanos", "10","14"));
+    }
+
 }
